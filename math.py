b1cd99452e05092cc9fda56b68499af8cd78e56e
ObraztsovaKsenia <ksynya_obr@mail.ru>
2024-07-02 12:35:19 +0300

    Прошли math

---
+++ math.py
@@ -0,0 +1,10 @@
+import math
+
+x = 36
+print(f"Округление вверх для {x}: {math.ceil(x)}")
+print(f"Округление вниз для {x}: {math.floor(x)}")
+print(f"Абсолютное число {x}: {math.fabs(x)}")
+print(f"Корень {x}: {math.sqrt(x)}")
+
+
+
